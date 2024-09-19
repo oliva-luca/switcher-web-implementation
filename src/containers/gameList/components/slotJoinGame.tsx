706e16d656bbaafd9b83b6e98ed93175cc10cfa9
Joaquin Dubois <joaquin.dubois@mi.unc.edu.ar>
2024-09-19 11:06:20 -0300

    SWT-17 colores, id de partida y lista dinamica

--- src/containers/gameList/components/slotJoinGame.tsx
+++ src/containers/gameList/components/slotJoinGame.tsx
@@ -1,8 +1,17 @@
 import './slotJoinGame.css'
+// import 'bootstrap/dist/css/bootstrap-grid.css'
 
-export function SlotJoinGame(nombre: string, capacidad: number, capacidadActual: number){
+export interface GameProps {
+    id: number;
+    nombre: string;
+    capacidadActual: number;
+    capacidad: number;
+  }
+
+export function SlotJoinGame({ id, nombre, capacidadActual, capacidad }: GameProps){
     return (
         <article id='slotPartida'>
+            <span id='idPartida'>ID: {id}</span>
             <span id='nombrePartida'>{nombre}</span>
             <aside id='capacidadPartida'>{capacidadActual}/{capacidad}</aside> 
             <button id='botonUnirse'>Unirse</button>
--- src/containers/gameList/gameList.tsx
+++ src/containers/gameList/gameList.tsx
@@ -1,17 +1,27 @@
 import './gameList.css'
 import { SlotJoinGame } from './components/slotJoinGame'
 
+const partidas = [
+  { id: 1, nombre: 'Partida 1', capacidadActual: 2, capacidad: 2 },
+  { id: 2, nombre: 'Partida 2', capacidadActual: 2, capacidad: 4 },
+  { id: 3, nombre: 'Partida 3', capacidadActual: 4, capacidad: 4 },
+  { id: 4, nombre: 'Partida 4', capacidadActual: 2, capacidad: 3 },
+];
+
+
+
 export function GameList() {
 
-    return (
-
+  return (
     <div id="gameColumn">
-    {SlotJoinGame('Partida 1', 2, 2)}
-    {SlotJoinGame('Partida 2', 4, 2)}
-    {SlotJoinGame('Partida 3', 4, 4)}
-    {SlotJoinGame('Partida 4', 3, 2)}
+      {partidas.map((partida) => (
+        <SlotJoinGame
+          id={partida.id}
+          nombre={partida.nombre}
+          capacidadActual={partida.capacidadActual}
+          capacidad={partida.capacidad}
+        />
+      ))}
     </div>
-
-    )
-
-  }
+  );
+}
