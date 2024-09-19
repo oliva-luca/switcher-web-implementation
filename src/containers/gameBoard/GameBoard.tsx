fc092f9c24854046420bb659c8a6711ac04c3ad6
Luca Oliva <oliva.luca.g@gmail.com>
2024-09-19 02:09:53 -0300

    Colores no se mapean

--- src/containers/gameBoard/GameBoard.tsx
+++ src/containers/gameBoard/GameBoard.tsx
@@ -1,76 +1,37 @@
 import { MouseEvent } from "react";
 import "./GameBoard.css"
+import 'bootstrap/dist/css/bootstrap.css'
 
-function BoardTyle(){
+function BoardTyle({ color }){
     return (
-        <button className="boardTyle">
+        <button className={`btn btn-primary boardTyle `+color}>
         </button>
       );
 }
 
 function GameBoard(){
-    let items = ['ja','jaja','jajaja'];
-    //items = [];
+    const items: string[][] = [
+        ["red","red","red","red","red","red"],
+        ["blu","blu","blu","blu","blu","blu"],
+        ["grn","grn","grn","grn","grn","grn"],
+        ["ylw","ylw","ylw","ylw","ylw","ylw"],
+        ["red","red","red","red","red","red"],
+        ["red","red","red","red","red","red"]
+    ];
 
     const handleClick = (event: MouseEvent) => console.log(event);
 
     return (
-        <>
-
-
-        <div className="board">
-            <div className="column">
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
+        <div className="container text-center board">
+          {items.map((row, rowIndex) => (
+            <div key={rowIndex} className="column boardColumn">
+              {row.map((cell, cellIndex) => (
+                <BoardTyle key={cellIndex} color={cell} />
+              ))}
             </div>
-            <div className="column">
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-            </div>
-            <div className="column">
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-            </div>
-            <div className="column">
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-            </div>
-            <div className="column">
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-            </div>
-            <div className="column">
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-                <BoardTyle></BoardTyle>
-            </div>
+          ))}
         </div>
-
-        </>
-    );
+      );
 }
 
 export default GameBoard;
