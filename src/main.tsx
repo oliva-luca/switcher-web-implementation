828980d28eb863dc246a46b24bef93c5847e39ed
Mateo Ricci <mateo.ricci.v@gmail.com>
2024-09-24 01:30:02 -0300

    SWT-59 ticket terminado

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 // import Lobby from './containers/lobby/Lobby.tsx'; // Importa el componente Lobby
 import Game from './containers/game/Game.tsx'; // Importa el componente Game
+import Home from './containers/home/Home.tsx'; // Importa el componente Home
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
@@ -10,6 +11,7 @@ createRoot(document.getElementById('root')!).render(
     <Router>
       <div className='app'>
         <Routes>
+          <Route path="/" element={<Home />} /> {/* Ruta para la página de inicio */}
           {/* <Route path="/lobby" element={<Lobby />} /> */}
           <Route path="/game" element={<Game />} />
         </Routes>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/gamelist': {
+        target: 'http://127.0.0.1:8000/',
+        changeOrigin: true,
+        secure: false,
+      },
+      '/user': {
+        target: 'http://127.0.0.1:8000/',
+        changeOrigin: true,
+        secure: false,
+      },
+    },
+  },
+});
