fab9bb1b6067ab59a79037f2f455d5f237474019
Luca Oliva <oliva.luca.g@gmail.com>
2024-09-29 13:41:10 -0300

    SWT-45 grid para colocar el tablero de figuras de cada gugador agregado

--- src/containers/game/Game.tsx
+++ src/containers/game/Game.tsx
@@ -1,24 +1,36 @@
-import "./Game.css"
-import "./Game.css"
-import GameBoard from './components/GameBoard'
+import "./Game.css";
+import "./Game.css";
+import GameBoard from "./components/GameBoard";
 
 function Game() {
+  const board: string[][] = [
+    ["red", "ylw", "grn", "blu", "red", "ylw"],
+    ["blu", "red", "ylw", "grn", "blu", "red"],
+    ["grn", "blu", "red", "ylw", "grn", "blu"],
+    ["ylw", "grn", "blu", "red", "ylw", "grn"],
+    ["red", "ylw", "grn", "blu", "red", "ylw"],
+    ["blu", "red", "ylw", "grn", "blu", ""],
+  ];
 
-    const board: string[][] = [
-        ["red", "ylw", "grn", "blu", "red", "ylw", "grn"],
-        ["blu", "red", "ylw", "grn", "blu", "red", "ylw"],
-        ["grn", "blu", "red", "ylw", "grn", "blu", "red"],
-        ["ylw", "grn", "blu", "red", "ylw", "grn", "blu"],
-        ["red", "ylw", "grn", "blu", "red", "ylw", "grn"],
-        ["blu", "red", "ylw", "grn", "blu", "red", "ylw"],
-    ];
+  return (
+    <>
+      <div className="board">
+        <div className="square-cor">00</div>
+        <div className="square-side2">01</div>
+        <div className="square-cor">02</div>
 
-    return(
-        <>
-        <div className="game-form"></div>
-            <GameBoard board={board}/>
-        </>
-    )
+        <div className="square-side">10</div>
+        <div>
+          <GameBoard board={board} />
+        </div>
+        <div className="square-side">12</div>
+
+        <div className="square-cor">20</div>
+        <div className="square-side2">21</div>
+        <div className="square-cor">22</div>
+      </div>
+    </>
+  );
 }
 
-export default Game
+export default Game;
