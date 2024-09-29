533bd0e96363b1123716c05dfd93407e7ff8ded6
Luca Oliva <oliva.luca.g@gmail.com>
2024-09-29 19:47:38 -0300

    SWT44 FigureBoard agregado

--- src/containers/game/Game.tsx
+++ src/containers/game/Game.tsx
@@ -1,24 +1,45 @@
-import "./Game.css"
-import "./Game.css"
-import GameBoard from './components/GameBoard'
+import "./Game.css";
+import "./Game.css";
+import GameBoard from "./components/GameBoard";
+import FigureBoard from "./components/FigureBoard";
 
 function Game() {
+  const board: string[][] = [
+    ["red", "ylw", "grn", "blu", "red", "ylw"],
+    ["blu", "red", "ylw", "grn", "blu", "red"],
+    ["grn", "blu", "red", "ylw", "grn", "blu"],
+    ["ylw", "grn", "blu", "red", "ylw", "grn"],
+    ["red", "ylw", "grn", "blu", "red", "ylw"],
+    ["blu", "red", "ylw", "grn", "blu", ""],
+  ];
 
-    const board: string[][] = [
-        ["red", "ylw", "grn", "blu", "red", "ylw", "grn"],
-        ["blu", "red", "ylw", "grn", "blu", "red", "ylw"],
-        ["grn", "blu", "red", "ylw", "grn", "blu", "red"],
-        ["ylw", "grn", "blu", "red", "ylw", "grn", "blu"],
-        ["red", "ylw", "grn", "blu", "red", "ylw", "grn"],
-        ["blu", "red", "ylw", "grn", "blu", "red", "ylw"],
-    ];
+  return (
+    <>
+      <div className="board">
+        <div></div>
+        <div>
+          <FigureBoard pos="top" deck={3} />
+        </div>
+        <div></div>
 
-    return(
-        <>
-        <div className="game-form"></div>
-            <GameBoard board={board}/>
-        </>
-    )
+        <div>
+          <FigureBoard pos="lft" deck={3} />
+        </div>
+        <div>
+          <GameBoard board={board} />
+        </div>
+        <div>
+          <FigureBoard pos="rgt" deck={3} />
+        </div>
+
+        <div></div>
+        <div>
+          <FigureBoard pos="btm" deck={3} />
+        </div>
+        <div></div>
+      </div>
+    </>
+  );
 }
 
-export default Game
+export default Game;
