9f49612b0368cbaf8c50a78743ef06d325c3cb8e
Luca Oliva <oliva.luca.g@gmail.com>
2024-09-29 17:24:27 -0300

    SWT-45 componente mazo de figuras agrgado + placeholder para figuras

--- src/containers/game/components/FigureBoard.tsx
+++ src/containers/game/components/FigureBoard.tsx
@@ -8,10 +8,38 @@ interface FigureBoardProp {
 const FigureBoard = ({ pos, deck }: FigureBoardProp) => {
   return (
     <>
-      {pos == "btm" && <div className="figureBoard ply1">{deck}</div>}
-      {pos == "rgt" && <div className="figureBoardSide ply2">{deck}</div>}
-      {pos == "top" && <div className="figureBoard ply3">{deck}</div>}
-      {pos == "lft" && <div className="figureBoardSide ply4">{deck}</div>}
+      {pos == "btm" && (
+        <div className="figureBoard figureGridBtm">
+          <div className="figureDeck ply1">{deck}</div>
+          <div className="figureDeck"></div>
+          <div className="figureDeck"></div>
+          <div className="figureDeck"></div>
+        </div>
+      )}
+      {pos == "rgt" && (
+        <div className="figureBoardSide figureGridRgt">
+          <div className="figureDeck"></div>
+          <div className="figureDeck"></div>
+          <div className="figureDeck"></div>
+          <div className="figureDeck ply2">{deck}</div>
+        </div>
+      )}
+      {pos == "top" && (
+        <div className="figureBoard figureGridTop">
+          <div className="figureDeck"></div>
+          <div className="figureDeck"></div>
+          <div className="figureDeck"></div>
+          <div className="figureDeck ply3">{deck}</div>
+        </div>
+      )}
+      {pos == "lft" && (
+        <div className="figureBoardSide figureGridLft">
+          <div className="figureDeck ply4">{deck}</div>
+          <div className="figureDeck"></div>
+          <div className="figureDeck"></div>
+          <div className="figureDeck"></div>
+        </div>
+      )}
     </>
   );
 };
