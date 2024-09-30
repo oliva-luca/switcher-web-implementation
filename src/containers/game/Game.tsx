3973a16c94dea705536d762691bf3c1170414183
Luca Oliva <oliva.luca.g@gmail.com>
2024-09-30 17:50:49 -0300

    SWT-44 conectadas al back y mostradas segun orden de jugadores

--- src/containers/game/Game.tsx
+++ src/containers/game/Game.tsx
@@ -1,66 +1,148 @@
-<<<<<<< HEAD
-import "./Game.css";
-import "./Game.css";
-import GameBoard from "./components/GameBoard";
-import FigureBoard from "./components/FigureBoard";
-=======
+import axios from "axios";
+import { useEffect, useState } from "react";
 import React from "react";
 import "./Game.css";
 import FigureBoard from "./components/mainBoard/FigureBoard";
 import GameBoard from "./components/mainBoard/GameBoard";
->>>>>>> 2ba7d57c095e4830d2778fbe1254bdb44c7b7b20
+
+const board2: string[][] = [
+  ["red", "ylw", "grn", "blu", "red", "ylw"],
+  ["blu", "red", "ylw", "grn", "blu", "red"],
+  ["grn", "blu", "red", "ylw", "grn", "blu"],
+  ["ylw", "grn", "blu", "red", "ylw", "grn"],
+  ["red", "ylw", "grn", "blu", "red", "ylw"],
+  ["blu", "red", "ylw", "grn", "blu", ""],
+];
+
+interface Player {
+  id_jugador: number;
+  block: boolean;
+  id_partida: number;
+  nombre: string;
+  in_game: boolean;
+  position: number;
+}
+
+interface MovCard {
+  id_partida: number;
+  type: number;
+  id_movcard: number;
+  id_jugador: number;
+}
+
+interface FigCard {
+  type: number;
+  show: boolean;
+  id_jugador: number;
+  id_figcard: number;
+  id_partida: number;
+}
+
+interface GameData {
+  id_partida: number;
+  name: string;
+  started: boolean;
+  password: string;
+  owner: string;
+  cant_jugadores: number;
+  is_private: boolean;
+  turn: number;
+  id_tablero: number;
+  players: Player[];
+  movcards: MovCard[];
+  figcards: FigCard[];
+}
+
+function ParsePlayers(players: Player[]) {
+  var order = new Array(players.length);
+  order[0] = players.find(
+    (ply) => ply.id_jugador.toString() == localStorage.getItem("userId")
+  );
+  for (let i = 1; i < players.length; i++) {
+    order[i] = players.find(
+      (ply) =>
+        ply.position == order[i - 1].position + 1 ||
+        (order[i - 1].position == 3 && ply.position == 0)
+    );
+  }
+  return order;
+}
+
+function ParsePlayerFigCards(plyId: number, figcards: FigCard[]) {
+  return figcards
+    .filter((fig) => fig.id_jugador == plyId)
+    .map((fig) => fig.type);
+}
+
+function ParsePlayerFigDeck(plyId: number, figcards: FigCard[]) {
+  return (
+    figcards.filter((fig) => !fig.show && fig.id_jugador == plyId).length - 3
+  );
+}
 
 function Game() {
-  const board: string[][] = [
-    ["red", "ylw", "grn", "blu", "red", "ylw"],
-    ["blu", "red", "ylw", "grn", "blu", "red"],
-    ["grn", "blu", "red", "ylw", "grn", "blu"],
-    ["ylw", "grn", "blu", "red", "ylw", "grn"],
-    ["red", "ylw", "grn", "blu", "red", "ylw"],
-    ["blu", "red", "ylw", "grn", "blu", ""],
-  ];
+  const [game, setGame] = useState<GameData | null>(null);
+
+  useEffect(() => {
+    const gameId = localStorage.getItem("gameId");
+
+    axios
+      .get(`/gamelist/${gameId}`)
+      .then((response) => {
+        setGame(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching the game list:", error);
+      });
+  }, []);
+
+  const order = game == null ? [] : ParsePlayers(game?.players);
 
   return (
     <>
-      <div className="board">
-        <div></div>
-        <div>
-<<<<<<< HEAD
-          <FigureBoard pos="top" deck={3} />
-=======
-          <FigureBoard pos="top" deck={0} />
->>>>>>> 2ba7d57c095e4830d2778fbe1254bdb44c7b7b20
-        </div>
-        <div></div>
+      {game == null ? (
+        <h1>Error creando menu de partida</h1>
+      ) : (
+        <div className="board">
+          <div></div>
+          <div>
+            <FigureBoard
+              pos="top"
+              deck={ParsePlayerFigDeck(order[2].id_jugador, game.figcards)}
+              cards={ParsePlayerFigCards(order[2].id_jugador, game.figcards)}
+            />
+          </div>
+          <div></div>
 
-        <div>
-<<<<<<< HEAD
-          <FigureBoard pos="lft" deck={3} />
-=======
-          <FigureBoard pos="lft" deck={0} />
->>>>>>> 2ba7d57c095e4830d2778fbe1254bdb44c7b7b20
-        </div>
-        <div>
-          <GameBoard board={board} />
-        </div>
-        <div>
-<<<<<<< HEAD
-          <FigureBoard pos="rgt" deck={3} />
-=======
-          <FigureBoard pos="rgt" deck={0} />
->>>>>>> 2ba7d57c095e4830d2778fbe1254bdb44c7b7b20
-        </div>
+          <div>
+            <FigureBoard
+              pos="lft"
+              deck={ParsePlayerFigDeck(order[1].id_jugador, game.figcards)}
+              cards={ParsePlayerFigCards(order[1].id_jugador, game.figcards)}
+            />
+          </div>
+          <div>
+            <GameBoard board={board2} />
+          </div>
+          <div>
+            <FigureBoard
+              pos="rgt"
+              deck={ParsePlayerFigDeck(order[3].id_jugador, game.figcards)}
+              cards={ParsePlayerFigCards(order[3].id_jugador, game.figcards)}
+            />
+          </div>
 
-        <div></div>
-        <div>
-<<<<<<< HEAD
-          <FigureBoard pos="btm" deck={3} />
-=======
-          <FigureBoard pos="btm" deck={0} />
->>>>>>> 2ba7d57c095e4830d2778fbe1254bdb44c7b7b20
+          <div></div>
+          <div>
+            <FigureBoard
+              pos="btm"
+              deck={ParsePlayerFigDeck(order[0].id_jugador, game.figcards)}
+              cards={ParsePlayerFigCards(order[0].id_jugador, game.figcards)}
+            />
+          </div>
+          <div></div>
         </div>
-        <div></div>
-      </div>
+      )}
     </>
   );
 }
--- src/containers/game/components/mainBoard/FigureBoard.tsx
+++ src/containers/game/components/mainBoard/FigureBoard.tsx
@@ -1,44 +1,45 @@
-import React from "react";
 import "./FigureBoard.css";
+import FigureCard from "./FigureCard";
 
 interface FigureBoardProp {
   pos: string;
   deck: number;
+  cards: number[];
 }
 
-const FigureBoard = ({ pos, deck }: FigureBoardProp) => {
+const FigureBoard = ({ pos, deck, cards }: FigureBoardProp) => {
   return (
     <>
       {pos == "btm" && (
         <div className="figureBoard figureGridBtm ply1">
           <div className="figureDeck ply1">{deck}</div>
-          <div className="figureDeck"></div>
-          <div className="figureDeck"></div>
-          <div className="figureDeck"></div>
+          <FigureCard card={cards[0]} />
+          <FigureCard card={cards[1]} />
+          <FigureCard card={cards[2]} />
         </div>
       )}
       {pos == "rgt" && (
         <div className="figureBoardSide figureGridRgt ply2">
-          <div className="figureDeck"></div>
-          <div className="figureDeck"></div>
-          <div className="figureDeck"></div>
+          <FigureCard card={cards[0]} />
+          <FigureCard card={cards[1]} />
+          <FigureCard card={cards[2]} />
           <div className="figureDeck ply2">{deck}</div>
         </div>
       )}
       {pos == "top" && (
         <div className="figureBoard figureGridTop ply3">
-          <div className="figureDeck"></div>
-          <div className="figureDeck"></div>
-          <div className="figureDeck"></div>
+          <FigureCard card={cards[0]} />
+          <FigureCard card={cards[1]} />
+          <FigureCard card={cards[2]} />
           <div className="figureDeck ply3">{deck}</div>
         </div>
       )}
       {pos == "lft" && (
         <div className="figureBoardSide figureGridLft ply4">
           <div className="figureDeck ply4">{deck}</div>
-          <div className="figureDeck"></div>
-          <div className="figureDeck"></div>
-          <div className="figureDeck"></div>
+          <FigureCard card={cards[0]} />
+          <FigureCard card={cards[1]} />
+          <FigureCard card={cards[2]} />
         </div>
       )}
     </>
