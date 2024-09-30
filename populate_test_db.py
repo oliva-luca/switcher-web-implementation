c30a42b28db4109804e30da242900d57c1b11ed5
Saantiago Miranda <santiago.miranda@mi.unc.edu.ar>
2024-09-29 22:53:53 -0300

    SWT-33 unitest getboardbyid

--- populate_test_db.py
+++ populate_test_db.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import sessionmaker
-from models import Game, engine, Player
+from models import Game, engine, Player, Tablero
 
 
 Session = sessionmaker(bind=engine)
@@ -20,7 +20,267 @@ def load_data_for_test():
         (4, 'player4', False, False, False, None),
         (5, 'player5', False, False, False, None),
     ]
-    
+
+    tableros = [( None, 1 )]
+
+
+    casillas = [{
+      "verde",
+      1,
+      0,
+      0,
+      1
+    },
+    {
+      "amarillo",
+      2,
+      0,
+      1,
+      1
+    },
+    {
+      "rojo",
+      3,
+      0,
+      2,
+      1
+    },
+    {
+      "azul",
+      4,
+      0,
+      3,
+      1
+    },
+    {
+      "amarillo",
+      5,
+      0,
+      4,
+      1
+    },
+    {
+      "amarillo",
+      6,
+      0,
+      5,
+      1
+    },
+    {
+      "amarillo",
+      7,
+      1,
+      0,
+      1
+    },
+    {
+      "amarillo",
+      8,
+      1,
+      1,
+      1
+    },
+    {
+      "amarillo",
+      9,
+      1,
+      2,
+      1
+    },
+    {
+      "azul",
+      10,
+      1,
+      3,
+      1
+    },
+    {
+      "verde",
+      11,
+      1,
+      4,
+      1
+    },
+    {
+      "verde",
+      12,
+      1,
+      5,
+      1
+    },
+    {
+      "azul",
+      13,
+      2,
+      0,
+      1
+    },
+    {
+      "rojo",
+      14,
+      2,
+      1,
+      1
+    },
+    {
+      "azul",
+      15,
+      2,
+      2,
+      1
+    },
+    {
+      "verde",
+      16,
+      2,
+      3,
+      1
+    },
+    {
+      "rojo",
+      17,
+      2,
+      4,
+      1
+    },
+    {
+      "rojo",
+      18,
+      2,
+      5,
+      1
+    },
+    {
+      "azul",
+      19,
+      3,
+      0,
+      1
+    },
+    {
+      "verde",
+      20,
+      3,
+      1,
+      1
+    },
+    {
+      "azul",
+      21,
+      3,
+      2,
+      1
+    },
+    {
+      "rojo",
+      22,
+      3,
+      3,
+      1
+    },
+    {
+      "amarillo",
+      23,
+      3,
+      4,
+      1
+    },
+    {
+      "rojo",
+      24,
+      3,
+      5,
+      1
+    },
+    {
+      "verde",
+      25,
+      4,
+      0,
+      1
+    },
+    {
+      "verde",
+      26,
+      4,
+      1,
+      1
+    },
+    {
+      "rojo",
+      27,
+      4,
+      2,
+      1
+    },
+    {
+      "azul",
+      28,
+      4,
+      3,
+      1
+    },
+    {
+      "amarillo",
+      29,
+      4,
+      4,
+      1
+    },
+    {
+      "verde",
+      30,
+      4,
+      5,
+      1
+    },
+    {
+      "azul",
+      31,
+      5,
+      0,
+      1
+    },
+    {
+      "rojo",
+      32,
+      5,
+      1,
+      1
+    },
+    {
+      "rojo",
+      33,
+      5,
+      2,
+      1
+    },
+    {
+      "verde",
+      34,
+      5,
+      3,
+      1
+    },
+    {
+      "amarillo",
+      35,
+      5,
+      4,
+      1
+    },
+    {
+      "azul",
+      36,
+      5,
+      5,
+      1
+    }
+
+
+    ]
+
+
     session = Session()
     try:    
         if session.query(Game).count() == 0:
@@ -38,6 +298,17 @@ def load_data_for_test():
             for id_jugador, nombre, in_game, block, turn, id_partida in players:
                 player = Player(id_jugador=id_jugador, nombre=nombre, in_game=in_game, block=block, turn=turn, id_partida=id_partida)
                 session.add(player)
+            session.commit()
+    finally:
+        session.close()
+
+    
+    session = Session()
+    try: 
+        if session.query(Tablero).count() == 0:
+            for color_principal,id_tablero in tableros:     
+                tablero = Tablero(color_principal=color_principal, id_tablero = id_tablero, casillas = casillas)
+                session.add(tablero)
             session.commit()
     finally:
         session.close()
--- test_end_to_end.py
+++ test_end_to_end.py
@@ -1,5 +1,6 @@
 import requests
 import pytest
+from board_to_test_board import tablero_a
 
 SERVICE_URL = 'http://localhost:8000'
 
@@ -12,8 +13,17 @@ def list_of_games():
         {'id_partida': 4, 'name': 'game4', 'cant_jugadores': 3, 'started': False, 'is_private': False, 'password': 'as', 'id_tablero': 4, 'players': []},
         {'id_partida': 5, 'name': 'game5', 'cant_jugadores': 2, 'started': False, 'is_private': False, 'password': 'as', 'id_tablero': 5, 'players': []},
     ]
+
+
+
     
 @pytest.mark.end2end_test
 def test_get_games_end_point(list_of_games):
     data = requests.get(f'{SERVICE_URL}/gamelist')
     assert data.json() == list_of_games
+
+
+@pytest.mark.end2end_test
+def test_get_tablero_by_id_endpoint(tablero_a):
+    data = requests.get(f'{SERVICE_URL}/tableros/1')
+    assert data.json() == tablero_a  
