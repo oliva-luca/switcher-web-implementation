a931e579e9f954b6179458b3c2acfbc95b182318
Joaquin Dubois <joaquin.dubois@mi.unc.edu.ar>
2024-09-30 00:23:15 -0300

    SWT-17 falta websocket para mergear a dev

--- src/containers/lobby/gameList/components/slotJoinGame.tsx
+++ src/containers/lobby/gameList/components/slotJoinGame.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react';
+import React from 'react';
+import Swal from 'sweetalert2';
 import './slotJoinGame.css'
-// import 'bootstrap/dist/css/bootstrap-grid.css'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom'; // Importar useNavigate
 
 export interface GameProps {
     id: number;
@@ -17,8 +19,42 @@ function restoreBackground(e) {
     e.target.style.background = '#7eb65b';
 }
 
-export function SlotJoinGame({ id, name, currentCapacity, capacity }: GameProps){
+export default function SlotJoinGame({ id, name, currentCapacity, capacity }: GameProps){
 
+    const navigate = useNavigate();
+
+    const joinGame = async (id: number) => {
+        try {
+            const userId = localStorage.getItem('userId');
+            console.log('Joining game');
+            const joinData = {
+                player_id : userId,
+            };
+
+            const joinQueryString = new URLSearchParams(joinData as any).toString();
+            const response = await axios.put(`/gamelist/join/${id}?${joinQueryString}`, null, {
+                headers: {
+                'Content-Type': 'application/json',
+                },
+            });
+            navigate('/game');
+        } catch (error) {
+            console.error('Error joining game:', error);
+            Swal.fire({
+                icon: 'error',
+                title: 'Error',
+                text: 'Hubo un error al unirse al juego. Por favor, inténtalo de nuevo.',
+            });
+        }
+    };
+    let full;
+    if (currentCapacity >= capacity){
+        full = true;
+    }
+    else{
+        full = false;
+    }
+    
     return (
         <article id='slotPartida'>
 
@@ -28,9 +64,13 @@ export function SlotJoinGame({ id, name, currentCapacity, capacity }: GameProps)
             
             <aside id='capacidadPartida'>{currentCapacity}/{capacity}</aside> 
             
-            <button id='botonUnirse'    onMouseOver={changeBackground}
-                                        onMouseLeave={restoreBackground}>
-                                        Unirse</button>
+            <button id='botonUnirse'
+                    onMouseOver={full ? undefined : changeBackground}
+                    onMouseLeave={full ? undefined : restoreBackground}
+                    onClick={full ? undefined : () => joinGame(id)}
+                    style={{ background: full ? '#9b9b9b' : '#7eb65b', cursor: full ? 'not-allowed' : 'pointer' }}
+                    disabled={full}
+                    >{full ? 'Lleno' : 'Unirse'}</button>
         </article>
     )
 }
