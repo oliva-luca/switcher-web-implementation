184a72f85113366b2499d56dbeb8d1a59d60dd25
Mateo Caaranza Velez <mateocvelez@mi.unc.edu.ar>
2024-09-30 02:09:41 -0300

    tests de cambiar el turno

--- populate_test_db.py
+++ populate_test_db.py
@@ -6,19 +6,19 @@ Session = sessionmaker(bind=engine)
 
 def load_data_for_test():
     games = [
-        (1, 'game1', 2, False, False, 'as', 1),
-        (2, 'game2', 3, True, False, 'as', None),
-        (3, 'game3', 4, False, False, 'as', None),
-        (4, 'game4', 3, False, False, 'as', None),
-        (5, 'game5', 2, False, False, 'as', None),
+        (1, 'game1', 1, False, False, 'as', None, 1),
+        (2, 'game2', 3, True, False, 'as', None, None),
+        (3, 'game3', 4, False, False, 'as', None, None),
+        (4, 'game4', 3, False, False, 'as', None, None),
+        (5, 'game5', 2, True, False, 'as', 4, None),
     ]
     
-    players =[
-        (1, 'player1', False, False, False, None),
-        (2, 'player2', False, False, False, None),
-        (3, 'player3', False, False, False, None),
-        (4, 'player4', False, False, False, None),
-        (5, 'player5', False, False, False, None),
+    players = [
+        (1, 'player1', False, False, None, None),
+        (2, 'player2', False, False, None, None),
+        (3, 'player3', False, False, None, None),
+        (4, 'player4', False, False, 0, 5),
+        (5, 'player5', False, False, 1, 5),
     ]
 
     tableros = [( None, 1 )]
@@ -65,8 +65,8 @@ def load_data_for_test():
     session = Session()
     try:    
         if session.query(Game).count() == 0:
-            for id_partida, name, cant_jugadores, started, is_private, password, id_tablero in games:
-                game = Game(id_partida=id_partida, name=name, cant_jugadores=cant_jugadores, started=started, is_private=is_private, password=password, id_tablero=id_tablero)
+            for id_partida, name, cant_jugadores, started, is_private, password, turn, id_tablero in games:
+                game = Game(id_partida=id_partida, name=name, cant_jugadores=cant_jugadores, started=started, is_private=is_private, password=password, turn=turn, id_tablero=id_tablero)
                 session.add(game)
             session.commit()
     finally:
@@ -76,8 +76,8 @@ def load_data_for_test():
     
     try:
         if session.query(Player).count() == 0:
-            for id_jugador, nombre, in_game, block, turn, id_partida in players:
-                player = Player(id_jugador=id_jugador, nombre=nombre, in_game=in_game, block=block, turn=turn, id_partida=id_partida)
+            for id_jugador, nombre, in_game, block, position, id_partida in players:
+                player = Player(id_jugador=id_jugador, nombre=nombre, in_game=in_game, block=block, position=position, id_partida=id_partida)
                 session.add(player)
             session.commit()
     finally:
--- test_end_to_end.py
+++ test_end_to_end.py
@@ -7,11 +7,13 @@ SERVICE_URL = 'http://localhost:8000'
 @pytest.fixture
 def list_of_games():
     return [
-        {'id_partida': 1, 'name': 'game1', 'cant_jugadores': 2, 'started': False, 'is_private': False, 'password': 'as', 'id_tablero': 1, 'players': [], 'movcards' : []},
-        {'id_partida': 2, 'name': 'game2', 'cant_jugadores': 3, 'started': True, 'is_private': False, 'password': 'as', 'id_tablero': None, 'players': [], 'movcards' : []},
-        {'id_partida': 3, 'name': 'game3', 'cant_jugadores': 4, 'started': False, 'is_private': False, 'password': 'as', 'id_tablero': None, 'players': [], 'movcards' : []},
-        {'id_partida': 4, 'name': 'game4', 'cant_jugadores': 3, 'started': False, 'is_private': False, 'password': 'as', 'id_tablero': None, 'players': [], 'movcards' : []},
-        {'id_partida': 5, 'name': 'game5', 'cant_jugadores': 2, 'started': False, 'is_private': False, 'password': 'as', 'id_tablero': None, 'players': [], 'movcards' : []},
+        {'id_partida': 1, 'name': 'game1', 'cant_jugadores': 1, 'started': False, 'is_private': False, 'password': 'as', 'turn' : None, 'id_tablero': 1, 'players': [], 'movcards' : []},
+        {'id_partida': 2, 'name': 'game2', 'cant_jugadores': 3, 'started': True, 'is_private': False, 'password': 'as', 'turn' : None, 'id_tablero': None, 'players': [], 'movcards' : []},
+        {'id_partida': 3, 'name': 'game3', 'cant_jugadores': 4, 'started': False, 'is_private': False, 'password': 'as', 'turn' : None, 'id_tablero': None, 'players': [], 'movcards' : []},
+        {'id_partida': 4, 'name': 'game4', 'cant_jugadores': 3, 'started': False, 'is_private': False, 'password': 'as', 'turn' : None, 'id_tablero': None, 'players': [], 'movcards' : []},
+        {'id_partida': 5, 'name': 'game5', 'cant_jugadores': 2, 'started': True, 'is_private': False, 'password': 'as', 'turn' : 4, 'id_tablero': None, 'players': [
+                {'block': False,'id_jugador': 4,'id_partida': 5,'in_game': False,'nombre': 'player4','position': 0,},
+                {'block': False,'id_jugador': 5,'id_partida': 5,'in_game': False,'nombre': 'player5','position': 1,},], 'movcards' : []},
     ]
 
 
