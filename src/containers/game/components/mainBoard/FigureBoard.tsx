842ec8e5c8781a65586a6b4465b6af46a0d4bcd3
Luca Oliva <oliva.luca.g@gmail.com>
2024-10-09 22:55:52 -0300

    SWT-88 testing + dev merge

--- src/containers/game/components/mainBoard/FigureBoard.tsx
+++ src/containers/game/components/mainBoard/FigureBoard.tsx
@@ -12,7 +12,7 @@ const FigureBoard = ({ pos, deck, cards, name }: FigureBoardProp) => {
   return (
     <>
       {pos == "btm" && (
-        <div className="figureBoard figureGridBtm ply1">
+        <div className="figureBoard figureGrid ply1">
           <div className="playerName">{name}</div>
           <div className="figureDeck ply1">{deck}</div>
           <FigureCard card={cards[0]} />
@@ -21,7 +21,7 @@ const FigureBoard = ({ pos, deck, cards, name }: FigureBoardProp) => {
         </div>
       )}
       {pos == "rgt" && (
-        <div className="figureBoardSide figureGridRgt ply2">
+        <div className="figureBoardSide figureGridSide ply2">
           <FigureCard card={cards[0]} />
           <FigureCard card={cards[1]} />
           <FigureCard card={cards[2]} />
@@ -30,7 +30,7 @@ const FigureBoard = ({ pos, deck, cards, name }: FigureBoardProp) => {
         </div>
       )}
       {pos == "top" && (
-        <div className="figureBoard figureGridTop ply3">
+        <div className="figureBoard figureGrid ply3">
           <FigureCard card={cards[0]} />
           <FigureCard card={cards[1]} />
           <FigureCard card={cards[2]} />
@@ -39,7 +39,7 @@ const FigureBoard = ({ pos, deck, cards, name }: FigureBoardProp) => {
         </div>
       )}
       {pos == "lft" && (
-        <div className="figureBoardSide figureGridLft ply4">
+        <div className="figureBoardSide figureGridSide ply4">
           <div className="playerName">{name}</div>
           <div className="figureDeck ply4">{deck}</div>
           <FigureCard card={cards[0]} />
