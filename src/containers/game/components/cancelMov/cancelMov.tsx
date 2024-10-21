fcb5c0465fc64594123de013c09ed8a9b3c69250
Luca Oliva <oliva.luca.g@gmail.com>
2024-10-21 16:06:43 -0300

    HOTFIX tablero y cancelar mov

--- src/containers/game/components/cancelMov/cancelMov.tsx
+++ src/containers/game/components/cancelMov/cancelMov.tsx
@@ -1,29 +1,33 @@
-import React, { useEffect, useState } from 'react';
-import './cancelMov.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios'; // Importar Axios
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import "./cancelMov.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import axios from "axios"; // Importar Axios
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import { useCurrentPlay } from "../../hooks/CurrentPlay.context";
 
 const CancelMov = () => {
-    const cancel = async () => {
-        const gameId = localStorage.getItem('gameId');
-        try {
-            await axios.put(`/gamelist/cancelmoves/${gameId}`);
-        } catch (error) {
-            console.log(error);
-        }
+  const { currentTurn } = useCurrentPlay();
+  const cancel = async () => {
+    const gameId = localStorage.getItem("gameId");
+    if (currentTurn == localStorage.getItem("userId")) {
+      try {
+        await axios.put(`/gamelist/cancelmoves/${gameId}`);
+      } catch (error) {
+        console.log(error);
+      }
     }
-    
-    return (
-        <button 
-        type="submit" 
-        className="btn btn-lg w-20 bottom-cancel" 
-        onClick={cancel}>
-        CANCELAR MOVIMIENTOS
-        </button>
-    );
-    
-}
+  };
+
+  return (
+    <button
+      type="submit"
+      className="btn btn-lg w-20 bottom-cancel"
+      onClick={cancel}
+    >
+      CANCELAR MOVIMIENTOS
+    </button>
+  );
+};
 
 export default CancelMov;
