d834a2f028076c5fb59622d6095936f56c7dfb0e
Mateo Carranza Velez <mateocvelez@mi.unc.edu.ar>
2024-11-06 21:29:16 -0300

    SWT-154 desbloqueo de jugador implementado

--- operations.py
+++ operations.py
@@ -532,6 +532,7 @@ class Operations:
                 raise NotTheirTurnError(f"Player with ID {player.id_jugador} doesnt have the turn.")
             figcard.id_jugador = None
             figcard.shown = False
+            figcard.blocked = False
             await confirmar_cambios(session, game.id_tablero)
 
             actualizar_informacion_casillas(game_id, game.tablero, session)
@@ -539,6 +540,14 @@ class Operations:
             game.tablero.color_prohibido = color
 
             session.commit()
+
+            # Detectar si descartó la unica bloqueada que tenía
+            # El chequeo de que sea valida de jugar se hace desde el front
+            number_of_shown_figcards = session.query(FigCard).filter((FigCard.id_jugador == player.id_jugador)
+                                                                     & FigCard.shown).count()
+            if number_of_shown_figcards == 0 and player.blocked:
+                # Debe ser desbloqueado
+                player.blocked = True
 
             # Detectar si el jugador que descartó esta carta ganó
             number_of_figcards = session.query(FigCard).filter(FigCard.id_jugador == player.id_jugador).count()
