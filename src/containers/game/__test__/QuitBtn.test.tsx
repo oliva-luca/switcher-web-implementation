9806f430903e9ddab967fc206458b1b82e0de997
Luca Oliva <oliva.luca.g@gmail.com>
2024-11-11 00:44:10 -0300

    SWT-178 testing y documentacion de QuitBtn agregados

--- src/containers/game/__test__/QuitBtn.test.tsx
+++ src/containers/game/__test__/QuitBtn.test.tsx
@@ -2,10 +2,13 @@ import React from "react";
 import { render, fireEvent, waitFor } from "@testing-library/react";
 import { describe, it, expect, jest, beforeEach } from "@jest/globals";
 import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import QuitBtn from "../components/QuitBtn/QuitBtn"; // Ajusta la ruta de importación según sea necesario
+import { useNavigate, BrowserRouter } from "react-router-dom";
+import QuitBtn from "../components/QuitBtn/QuitBtn";
 
+// Mockeo de HTTP requests
 jest.mock("axios");
+
+// Mockeo de useNavigate
 jest.mock("react-router-dom", () => ({
   ...jest.requireActual("react-router-dom"),
   useNavigate: jest.fn(),
@@ -13,28 +16,43 @@ jest.mock("react-router-dom", () => ({
 
 describe("QuitBtn Component", () => {
   beforeEach(() => {
+    // Limpia sessionStorage y los mocks antes de cada test
     sessionStorage.clear();
     jest.clearAllMocks();
   });
 
   it("should render the button correctly", () => {
     const { getByRole } = render(<QuitBtn />);
+
+    // Revisa que de renderize de forma correcta
     expect(
       getByRole("button", { name: "ABANDONAR PARTIDA" })
     ).toBeInTheDocument();
   });
 
   it("should call quit function on button click", async () => {
+    // Mockea una funcion "navigate" para que useNavigate lo retorne
     const navigate = jest.fn();
     (useNavigate as jest.Mock).mockReturnValue(navigate);
+
+    // Asigna valor a playerId para que set use en el PUT
     sessionStorage.setItem("playerId", "1");
     (axios.put as jest.Mock).mockResolvedValueOnce({ data: {} });
 
-    const { getByRole } = render(<QuitBtn />);
+    const { getByRole } = render(
+      <BrowserRouter>
+        <QuitBtn />
+      </BrowserRouter>
+    );
+
+    // Clickea el boton para disparar el PUT y navigate
     fireEvent.click(getByRole("button", { name: "ABANDONAR PARTIDA" }));
 
     await waitFor(() => {
+      // Revisa que se haya mandado el PUT
       expect(axios.put).toHaveBeenCalledWith("/gamelist/leave/1");
+
+      // Revisa que se haya llamado a navigate
       expect(navigate).toHaveBeenCalledWith("/lobby");
     });
   });
--- src/containers/game/components/QuitBtn/QuitBtn.tsx
+++ src/containers/game/components/QuitBtn/QuitBtn.tsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useState } from "react";
 import "./QuitBtn.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import axios from "axios"; // Importar Axios
+import axios from "axios";
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 
+// Componente QuitBtn - Representa un botón para abandonar una partida
 const QuitBtn = () => {
-  const navigate = useNavigate();
-  const quit = () => {
+  const navigate = useNavigate(); // Hook para redirigir a otra ruta
+
+  // Handler del evento para abandonar partida
+  const handleQuit = () => {
     const userId = sessionStorage.getItem("playerId");
     try {
+      // PUT request para que el jugador abandone la partida
       const response = axios.put(`/gamelist/leave/${userId}`);
+
+      // Redireccion del usuario luego de abandonar
       navigate("/lobby");
     } catch (error) {
+      // Muestra el error en la consola en caso de que falle la solicitud
       console.log(error);
     }
   };
 
+  // Renderizacion del botón
   return (
     <button
-      type="submit"
-      className="btn btn-lg w-20 bottom-quit"
-      onClick={quit}
+      type="submit" // Tipo de botón
+      className="btn btn-lg w-20 bottom-quit" // Clases para aplicar estilo
+      onClick={handleQuit} // Llama a la función quit al hacer clic
     >
       ABANDONAR PARTIDA
     </button>
