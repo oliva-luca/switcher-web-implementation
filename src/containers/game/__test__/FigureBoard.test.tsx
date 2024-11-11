cb31cad2450f2f4006744940cfc8526d1168bd4d
Luca Oliva <oliva.luca.g@gmail.com>
2024-11-11 19:38:40 -0300

    SWT-178 testing y documentacion de FigureBoard

--- src/containers/game/__test__/FigureBoard.test.tsx
+++ src/containers/game/__test__/FigureBoard.test.tsx
@@ -4,11 +4,13 @@ import "@testing-library/jest-dom";
 import FigureBoard from "../components/mainBoard/FigureBoard";
 import { FigCard } from "../utils/interfaces";
 
+// Mock de FigureCard para que se renderice un div con el atributo data-testid="figure-card" en lugar del componente real
 jest.mock("../components/mainBoard/FigureCard", () => {
   return jest.fn(() => <div data-testid="figure-card"></div>);
 });
 
 describe("FigureBoard Component", () => {
+  // Datos simulados de cartas de figura
   const mockCards: FigCard[] = [
     {
       id_figcard: 1,
@@ -37,8 +39,14 @@ describe("FigureBoard Component", () => {
     render(
       <FigureBoard pos="btm" deck={10} cards={mockCards} name="Player 1" />
     );
+
+    // Verifica que el nombre del jugador sea visible en el documento
     expect(screen.getByText("Player 1")).toBeInTheDocument();
+
+    // Verifica que el número de cartas en el mazo sea visible en el documento
     expect(screen.getByText("10")).toBeInTheDocument();
+
+    // Verifica que se hayan renderizado 3 cartas de figura
     expect(screen.getAllByTestId("figure-card").length).toBe(3);
   });
 
@@ -46,8 +54,14 @@ describe("FigureBoard Component", () => {
     render(
       <FigureBoard pos="rgt" deck={20} cards={mockCards} name="Player 2" />
     );
+
+    // Verifica que el nombre del jugador sea visible en el documento
     expect(screen.getByText("Player 2")).toBeInTheDocument();
+
+    // Verifica que el número de cartas en el mazo sea visible en el documento
     expect(screen.getByText("20")).toBeInTheDocument();
+
+    // Verifica que se hayan renderizado 3 cartas de figura
     expect(screen.getAllByTestId("figure-card").length).toBe(3);
   });
 
@@ -55,8 +69,14 @@ describe("FigureBoard Component", () => {
     render(
       <FigureBoard pos="top" deck={30} cards={mockCards} name="Player 3" />
     );
+
+    // Verifica que el nombre del jugador sea visible en el documento
     expect(screen.getByText("Player 3")).toBeInTheDocument();
+
+    // Verifica que el número de cartas en el mazo sea visible en el documento
     expect(screen.getByText("30")).toBeInTheDocument();
+
+    // Verifica que se hayan renderizado 3 cartas de figura
     expect(screen.getAllByTestId("figure-card").length).toBe(3);
   });
 
@@ -64,8 +84,14 @@ describe("FigureBoard Component", () => {
     render(
       <FigureBoard pos="lft" deck={40} cards={mockCards} name="Player 4" />
     );
+
+    // Verifica que el nombre del jugador sea visible en el documento
     expect(screen.getByText("Player 4")).toBeInTheDocument();
+
+    // Verifica que el número de cartas en el mazo sea visible en el documento
     expect(screen.getByText("40")).toBeInTheDocument();
+
+    // Verifica que se hayan renderizado 3 cartas de figura
     expect(screen.getAllByTestId("figure-card").length).toBe(3);
   });
 });
--- src/containers/game/components/mainBoard/FigureBoard.tsx
+++ src/containers/game/components/mainBoard/FigureBoard.tsx
@@ -3,17 +3,34 @@ import "./FigureBoard.css";
 import FigureCard from "./FigureCard";
 import { FigCard } from "../../utils/interfaces";
 
+// Interfaz de propiedades del componente
 interface FigureBoardProp {
-  pos: string;
-  deck: number;
-  cards: FigCard[];
-  name: string;
+  pos: string; // Posición del tablero para tablero (puede ser 'btm', 'rgt', 'top', 'lft')
+  deck: number; // Número de cartas en el mazo del jugador
+  cards: FigCard[]; // Lista de cartas de figura del jugador
+  name: string; // Nombre del jugador
 }
 
+/**
+ * Componente del tablero de figuras
+ *
+ * Este componente muestra el tablero de figuras de un jugador en función de su posicion.
+ * Dependiendo de la posicion proporcionada (`pos`), se renderiza la interfaz del tablero
+ * con las cartas de figura del jugador, el numero de cartas en el mazo, y el nombre del jugador.
+ * Cada carta de figura es representada por el componente `FigureCard`.
+ *
+ * @param {FigureBoardProp}
+ * - `pos` (string): Posicion del tablero para la figura. Puede ser: 'btm' (abajo), 'rgt' (derecha), 'top' (arriba), 'lft' (izquierda).
+ * - `deck` (number): Numero de cartas en el mazo del jugador.
+ * - `cards` (FigCard[]): Lista de cartas de figura del jugador, donde cada carta tiene una serie de propiedades.
+ * - `name` (String): Nombre del jugador.
+ *
+ * @returns {JSX.Element} Tablero de figuras de un jugador.
+ */
 const FigureBoard = ({ pos, deck, cards, name }: FigureBoardProp) => {
   return (
     <>
-      {pos == "btm" && (
+      {pos === "btm" && (
         <div className="figureBoard figureGrid ply1">
           <div className="playerName">{name}</div>
           <div className="figureDeck ply1">{deck}</div>
@@ -28,7 +45,7 @@ const FigureBoard = ({ pos, deck, cards, name }: FigureBoardProp) => {
           ))}
         </div>
       )}
-      {pos == "rgt" && (
+      {pos === "rgt" && (
         <div className="figureBoardSide figureGridSide ply2">
           {cards.map((card) => (
             <FigureCard
@@ -43,7 +60,7 @@ const FigureBoard = ({ pos, deck, cards, name }: FigureBoardProp) => {
           <div className="playerName">{name}</div>
         </div>
       )}
-      {pos == "top" && (
+      {pos === "top" && (
         <div className="figureBoard figureGrid ply3">
           {cards.map((card) => (
             <FigureCard
@@ -58,7 +75,7 @@ const FigureBoard = ({ pos, deck, cards, name }: FigureBoardProp) => {
           <div className="playerName">{name}</div>
         </div>
       )}
-      {pos == "lft" && (
+      {pos === "lft" && (
         <div className="figureBoardSide figureGridSide ply4">
           <div className="playerName">{name}</div>
           <div className="figureDeck ply4">{deck}</div>
