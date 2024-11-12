b4c5c8ddc8f5802df6930d945aec6551b70c484c
Saantiago Miranda <santiago.miranda@mi.unc.edu.ar>
2024-11-11 21:18:37 -0300

    SWT-185 Fix para que no aparezca la partida luego de que alguien gane

--- operations.py
+++ operations.py
@@ -523,6 +523,7 @@ class Operations:
                 remaining_player.id_partida = None 
                 session.commit()
                 await manager_game.broadcast(id_game, f"winner {remaining_player.id_jugador}")
+                await manager.broadcast("Somebody won")
             else:
                 await manager_game.broadcast(id_game, "Player has left the game") 
             return {"message": f"Player {player_id} has left the game"}
